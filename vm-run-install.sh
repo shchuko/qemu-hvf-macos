fb804ef4bf8d9900bbe1caf83101747e7c870c31
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-02-23 21:39:20 +0300

    Refactor vm-run-install.sh
    Signed-off-by: Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>

--- vm-run-install.sh
+++ vm-run-install.sh
@@ -7,81 +7,112 @@
 # /Applications/Install macOS <OS NAME>.app required!
 #
 
-# OS name to install
-OS_NAME="Catalina"
+function setVars() {
+  # OS name to install
+  OS_NAME="Catalina"
 
-# Install media image
-INSTALL_IMG="BaseSystem.cdr"
+  # Don't attach install media image by default
+  INSTALL_OS_FLAG="False"
 
-# Creating drive to install macOS onto
-# 50G is enough for Catalina
-DRIVE_IMG="drive.qcow2"
-DRIVE_IMG_SIZE="50G"
-
-
-INSTALL_OS_FLAG="False"
-
-# building QEMU
-./qemu-build.sh
-QEMU_EXEC="./qemu/build/qemu-system-x86_64"
-if [[ -f "$QEMU_EXEC" ]]; then
-  echo "QEMU Executable: $QEMU_EXEC"
-else
-  echo "QEMU Executable not found"
-  exit 1
-fi
-
-# building readosk
-./readosk-build.sh
-READOSK_EXEC="./readosk/readosk"
-echo "readosk Executable: $READOSK_EXEC"
-if [[ -f "$READOSK_EXEC" ]]; then
-  echo "readosk Executable: $QEMU_EXEC"
-else
-  echo "readosk Executable not found"
-  exit 1
-fi
-
-if ! ./get-firmware.sh; then
-  echo "Firmware retrieval error occured"
-  exit 1
-fi
-
-while [[ $# -gt 0 ]]; do
-  case "$1" in
-  -install)
-    INSTALL_OS_FLAG="True"
-    ;;
-
-  -os)
-    OS_NAME="$2"
-    shift
-    ;;
-
-  *)
-    shift
-    ;;
-  esac
-  shift
-done
-
-# Creating install image
-if [[ "$INSTALL_OS_FLAG" == "True" ]]; then
-  if [[ ! -f "$INSTALL_IMG" ]]; then
-    export OS_NAME
-    if ! ./create-install-img.sh; then
-      echo "Creating install image failed"
-      exit 1
-    fi
-  fi
   # Creating drive to install macOS onto
-  [[ ! -f "$DRIVE_IMG_NAME" ]] && qemu-img create -f qcow2 "$DRIVE_IMG" "$DRIVE_IMG_SIZE"
+  # 50G is enough for Catalina
+  MAIN_DRIVE_IMG="drive.qcow2"
+  MAIN_DRIVE_IMG_SIZE="50G"
 
-  echo "Starting installation"
-  # Booting guest with install media attached
-  ./boot.sh -install -qemu "$QEMU_EXEC" -osk "$($READOSK_EXEC)"
-else
-  echo "Booting...."
-  # Booting guest without install media
-  ./boot.sh -qemu "$QEMU_EXEC" -osk "$($READOSK_EXEC)"
+  # Executables
+  QEMU_SYSTEM_X86_64="./qemu/build/qemu-system-x86_64"
+  QEMU_IMG="./qemu/build/qemu-img"
+  READOSK="./readosk/readosk"
+}
+
+function readArgs() {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+    -install)
+      INSTALL_OS_FLAG="True"
+      ;;
+
+    -os)
+      OS_NAME="$2"
+      shift
+      ;;
+
+    *)
+      shift
+      ;;
+    esac
+    shift
+  done
+}
+
+function loadComponents() {
+  # QEMU download and build
+  if ! ./qemu-build.sh; then
+    echo "QEMU build failed"
+    return 1
+  fi
+
+  # 'readosk' tool build
+  ./readosk-build.sh
+  if [[ ! -f "$READOSK" ]]; then
+    echo "readosk build failed"
+    return 1
+  fi
+
+  # QEMU Firmware download
+  if ! ./get-firmware.sh; then
+    echo "QEMU firmware download failed"
+    return 1
+  fi
+}
+
+function createInstallImg() {
+  export OS_NAME
+  ./create-install-img.sh
+  return $?
+}
+
+function createMainDrive() {
+  if [[ -f "$MAIN_DRIVE_IMG" ]]; then
+    return
+  fi
+
+  "$QEMU_IMG" create -f qcow2 "$MAIN_DRIVE_IMG" "$MAIN_DRIVE_IMG_SIZE"
+  return $?
+}
+
+function bootInstall() {
+  if ! createInstallImg; then
+    echo "Install image creation failed"
+    return 1
+  fi
+
+  ./boot.sh -install -qemu "$QEMU_SYSTEM_X86_64" -osk "$($READOSK)"
+}
+
+function bootNoInstall() {
+  ./boot.sh -qemu "$QEMU_SYSTEM_X86_64" -osk "$($READOSK)"
+}
+
+setVars
+readArgs "$@"
+
+if ! loadComponents; then
+  echo "Required components not present!"
+  exit 1
 fi
+
+if ! createMainDrive; then
+  echo "Drive creation failed"
+  exit 1
+fi
+
+if [[ "$INSTALL_OS_FLAG" == "True" ]]; then
+  bootInstall
+  EXITCODE=$?
+else
+  bootNoInstall
+  EXITCODE=$?
+fi
+
+exit $EXITCODE
