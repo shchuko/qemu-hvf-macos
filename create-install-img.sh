148cb386cf22aa4b8b8adacb4d102d5f49ff3473
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-02-24 07:26:52 +0300

    create-install-img.sh: more customization

--- create-install-img.sh
+++ create-install-img.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-set -euo pipefail
+#
+# Install media RAW image creation script
+# Defaults can be customized by setting:
+#  OS_NAME            - destination OS name, default: "Catalina"
+#  DESTDIR            - directory the image to be placed, default: "out"
+#  TARGET_IMAGE_NAME  - media image name, default: "BaseSystem.cdr"
+#
+set -xeuo pipefail
 
-# Install app can be changed by overriding OS_NAME variable
 OS_NAME="${OS_NAME:-Catalina}"
 MEDIA_CREATOR="/Applications/Install macOS $OS_NAME.app/Contents/Resources/createinstallmedia"
-
-# destination dir can be changed
-DESTDIR="${DESTDIR:-out}"
-IMAGE_NAME="$DESTDIR/BaseSystem"
-IMAGE_NAME_DMG="$IMAGE_NAME.dmg"
-IMAGE_NAME_RAW="$IMAGE_NAME.cdr"
-
+IMAGE_NAME="${DESTDIR:-out}/BaseSystem"
+TARGET_IMAGE_NAME="${TARGET_IMAGE_NAME:-$IMAGE_NAME.cdr}"
 MOUNTPOINT="/Volumes/install_build_$OS_NAME"
 DISK_DEV=""
+TTY="$(tty)"
 
-if [[ -f "$IMAGE_NAME_RAW" ]]; then
-  echo "$IMAGE_NAME_RAW already exists!"
+if [[ -f "$TARGET_IMAGE_NAME" ]]; then
   exit 0
 fi
 
@@ -37,21 +38,20 @@ cleanup() {
     hdiutil detach -force "$DISK_DEV" || true
   fi
 
-  if [[ -f "$IMAGE_NAME_DMG" ]]; then
-    rm "$IMAGE_NAME_DMG"
+  if [[ -f "$IMAGE_NAME.dmg" ]]; then
+    rm "$IMAGE_NAME.dmg"
   fi
 }
-trap cleanup EXIT
-trap cleanup INT
+trap cleanup EXIT INT
 cleanup
 
 mkdir -p "$DESTDIR"
 # here hdiutil appends ".dmg" suffix to given output name
 hdiutil create -o "$IMAGE_NAME" -size "$IMAGE_SIZE" -layout GPTSPUD -fs HFS+J
-DISK_DEV=$(hdiutil attach "$IMAGE_NAME_DMG" -noverify -mountpoint "$MOUNTPOINT" | awk 'NR==1 {print $1}')
+DISK_DEV=$(hdiutil attach "$IMAGE_NAME.dmg" -noverify -mountpoint "$MOUNTPOINT" | tee "$TTY" | awk 'NR==1 {print $1}')
 sudo "$MEDIA_CREATOR" --volume "$MOUNTPOINT" --nointeraction
 hdiutil detach "$DISK_DEV"
 
 # here hdiutil appends ".cdr" suffix to given output name
-hdiutil convert "$IMAGE_NAME_DMG" -format UDTO -o "$IMAGE_NAME"
-mv "$IMAGE_NAME.cdr" "$IMAGE_NAME_RAW"
+hdiutil convert "$IMAGE_NAME.dmg" -format UDTO -o "$IMAGE_NAME"
+mv "$IMAGE_NAME.cdr" "$TARGET_IMAGE_NAME"
