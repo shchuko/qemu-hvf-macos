3fe27f77d3e6acbc6aaad7a6238a26893068fb2b
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-08-31 22:16:19 +0300

    get-firmware.sh: use tar instead of zip
    Signed-off-by: Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>

--- get-firmware.sh
+++ get-firmware.sh
@@ -4,26 +4,30 @@ set -euo pipefail
 # Firmware retrieval script
 #
 
-ZIP_NAME="OvmfDarwinBin.zip"
+ARCHIVE_NAME="OvmfDarwinBin.tar.gz"
 TAG_NAME="v1.0-edk2-stable202105"
-URL="https://github.com/shchuko/OvmfDarwinPkg/releases/download/$TAG_NAME/$ZIP_NAME"
+URL="https://github.com/shchuko/OvmfDarwinPkg/releases/download/$TAG_NAME/$ARCHIVE_NAME"
 
 FIRMWARE_DIR="./Firmware"
 FIRMWARE_CODE="$FIRMWARE_DIR/OVMF_DARWIN_CODE.fd"
 FIRMWARE_VARS="$FIRMWARE_DIR/OVMF_DARWIN_VARS.fd"
 FIRMWARE_INFO="$FIRMWARE_DIR/FV_INFO.yaml"
 
-[[ ! -d "$FIRMWARE_DIR" ]] && mkdir -p "$FIRMWARE_DIR"
+if [[ ! -d "$FIRMWARE_DIR" ]]; then
+  mkdir "$FIRMWARE_DIR"
+fi
 
-if [[ -f "$FIRMWARE_CODE" ]] && [[ -f "$FIRMWARE_VARS" ]]; then
+if [[ -f "$FIRMWARE_CODE" ]] && [[ -f "$FIRMWARE_VARS" ]] && [[ -f "$FIRMWARE_INFO" ]]; then
   echo "Firmware retrieval skipped, locally exists: "
   cat "$FIRMWARE_INFO"
   exit 0
 fi
 
 wget "$URL"
-yes | unzip "$ZIP_NAME" -d "$FIRMWARE_DIR"
-rm "${ZIP_NAME:?}"
+
+
+tar -xf "$ARCHIVE_NAME" -C "$FIRMWARE_DIR"
+rm "${ARCHIVE_NAME:?}"
 
 echo "Firmware downloaded: $TAG_NAME"
-cat "$FIRMWARE_INFO"
+echo "$(cat $FIRMWARE_INFO)"
