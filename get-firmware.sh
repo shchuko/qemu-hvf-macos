cccb7be83c687726232efb9a396a66bec083f8f9
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-10-19 19:09:17 +0300

    Update get-firmware.sh

--- get-firmware.sh
+++ get-firmware.sh
@@ -4,30 +4,23 @@ set -euo pipefail
 # Firmware retrieval script
 #
 
-ARCHIVE_NAME="OvmfDarwinBin.tar.gz"
 TAG_NAME="v1.0-edk2-stable202105"
+ARCHIVE_NAME="ovmf-darwin-X64-RELEASE-$TAG_NAME.tar.gz"
 URL="https://github.com/shchuko/OvmfDarwinPkg/releases/download/$TAG_NAME/$ARCHIVE_NAME"
 
 FIRMWARE_DIR="./Firmware"
 FIRMWARE_CODE="$FIRMWARE_DIR/OVMF_DARWIN_CODE.fd"
 FIRMWARE_VARS="$FIRMWARE_DIR/OVMF_DARWIN_VARS.fd"
-FIRMWARE_INFO="$FIRMWARE_DIR/FV_INFO.yaml"
 
-if [[ ! -d "$FIRMWARE_DIR" ]]; then
-  mkdir "$FIRMWARE_DIR"
-fi
+mkdir -p "$FIRMWARE_DIR"
 
-if [[ -f "$FIRMWARE_CODE" ]] && [[ -f "$FIRMWARE_VARS" ]] && [[ -f "$FIRMWARE_INFO" ]]; then
+if [[ -f "$FIRMWARE_CODE" ]] && [[ -f "$FIRMWARE_VARS" ]]; then
   echo "Firmware retrieval skipped, locally exists: "
-  cat "$FIRMWARE_INFO"
   exit 0
 fi
 
 wget "$URL"
-
-
 tar -xf "$ARCHIVE_NAME" -C "$FIRMWARE_DIR"
 rm "${ARCHIVE_NAME:?}"
 
 echo "Firmware downloaded: $TAG_NAME"
-echo "$(cat $FIRMWARE_INFO)"
