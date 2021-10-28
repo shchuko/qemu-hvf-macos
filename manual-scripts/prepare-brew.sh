136676c4c67766a1a53cb0a0825a7a7222347fcf
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-10-28 22:46:40 +0300

    [manual-scripts] drop readosk from prepare-brew.sh

--- manual-scripts/prepare-brew.sh
+++ manual-scripts/prepare-brew.sh
@@ -3,7 +3,7 @@
 TAP_NAME="shchuko/qemu-macguest"
 brew tap "$TAP_NAME"
 brew update
-brew install "$TAP_NAME/readosk" "$TAP_NAME/ovmf-darwin" "$TAP_NAME/qemu"
+brew install "$TAP_NAME/ovmf-darwin" "$TAP_NAME/qemu"
 
 echo "#!/bin/bash
 LOOKUP_PREFIX=$(brew config | grep HOMEBREW_PREFIX | awk 'FS=" " {print $2}')
