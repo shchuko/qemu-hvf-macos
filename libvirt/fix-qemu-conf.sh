6c020a859ebf30a6106e5b8445f6aa8f94e6badd
Roman Grigorev <roman.grigorev@jetbrains.com>
2022-03-30 11:07:33 +0200

    [libvirt] fix-qemu-conf.sh - allow using virsh from non-root user

--- libvirt/fix-qemu-conf.sh
+++ libvirt/fix-qemu-conf.sh
@@ -5,3 +5,20 @@ PREFIX=$(brew config | grep HOMEBREW_PREFIX | awk 'FS=" " {print $2}')
 
 # Set dynamic_ownership=0 into qemu.conf
 sed -i '' 's/#dynamic_ownership[[:space:]]*=[[:space:]]*1/dynamic_ownership = 0/g' "$PREFIX/etc/libvirt/qemu.conf"
+
+
+
+LIBVIRTDFILE="$PREFIX/etc/libvirt/libvirtd.conf"
+LBDACKUPFILEBACKUPFILE="$LIBVIRTDFILE."$(date +%s)
+cp "$LIBVIRTDFILE" "$LBDACKUPFILEBACKUPFILE"
+# set  unix_sock_ro_perms = "0777"
+sed -i '' 's/#unix_sock_ro_perms/unix_sock_ro_perms/' "$LIBVIRTDFILE"
+# set  unix_sock_rw_perms = "0777"
+sed -i ''  's/#unix_sock_rw_perms/unix_sock_rw_perms/' "$LIBVIRTDFILE"
+
+LIBVIRTFILE="$PREFIX/etc/libvirt/libvirtd.conf"
+LBACKUPFILEBACKUPFILE="$LIBVIRTFILE."$(date +%s)
+cp "$LIBVIRTFILE" "$LBACKUPFILEBACKUPFILE"
+# set
+sed -i ''  's/#uri_default/uri_default/' "$LIBVIRTFILE"
+
